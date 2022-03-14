3c7a3e9ee502613c7256ded6027354b250688a93
Quentin Ikeno <ikenoq@gmail.com>
2022-03-13 15:44:36 -1000

    Create app.py

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, flash, redirect, render_template
+from flask_debugtoolbar import DebugToolbarExtension
+from models import db, connect_db
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "oh-so-secret"
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///pet_adoption_db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False
+
+debug = DebugToolbarExtension(app)
+
+connect_db(app)
+
+
+@app.route("/")
+def homepage():
+    """Show homepage."""
+    return render_template("index.html")
